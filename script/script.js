52ac8b6f5169eb2d5acc6181ffb0e15909fd19ab
Lukas Nolting <lukas.nolting@icloud.com>
2023-12-29 11:08:34 +0100

    mainpage

--- script/script.js
+++ script/script.js
@@ -1,15 +1,14 @@
 
-let currentPokemon;
+// let currentPokemon;
 
-async function loadPokemon(){
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander'
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    console.log('Loaded Pokemon', currentPokemon);
-    renderPokemonInfo();
-}
+// async function loadPokemon(){
+//     let url = 'https://pokeapi.co/api/v2/pokemon/charmander'
+//     let response = await fetch(url);
+//     currentPokemon = await response.json();
+//     renderPokemonInfo();
+// }
 
-function renderPokemonInfo(){
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById("pokemonImg").src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
-}
+// function renderPokemonInfo(){
+//     document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+//     document.getElementById("pokemonImg").src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+// }
