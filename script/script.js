fd8de87dd6bd34e8424aaa46817a404f5e821c29
Lukas Nolting <lukas.nolting@icloud.com>
2023-12-30 19:32:10 +0100

    colors cards + load more pokemon

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let responseAsJson = ``;
 let responseLength = 0;
-let maxPokemonRender = 20;
+let maxPokemonRender = 21;
 let loadPokemonNumber = 1;
 let pokemonAsJson = ``;
 
@@ -29,7 +29,7 @@ function loadMore() {
   let index = loadPokemonNumber + 20;
   loadPokemonNumber = index;
   maxPokemonRender = maxPokemonRender + 20;
-  renderPokemon(loadPokemonNumber);
+  renderPokemon(index);
 }
 
 async function renderPokemon(loadPokemonNumber) {
@@ -42,28 +42,33 @@ async function renderPokemon(loadPokemonNumber) {
     pokeImg =
       pokemonAsJson["sprites"]["other"]["official-artwork"]["front_default"];
     pokeType = pokemonAsJson["types"];
+    pokeId = pokemonAsJson["id"];
+    let color = pokemonAsJson["types"][0]["type"]["name"];
 
     let card = document.getElementById("card-container");
-    card.innerHTML += `<div class="card">
+    card.innerHTML += `<div class="card" id="pokeCard${i}" onclick="loadPokeValues(${pokeId}, ${pokeName})">
         <div class="card-head">
-            <div id="${pokeName}">${pokeName}</div>
-            <div id=""></div>
+            <div class='pokeName' id="${pokeName}">${pokeName}</div>
+            <div class='pokeId' id="${pokeId}">#${pokeId}</div>
         </div>
         <div class="card-content">
-            <div id="type${i}"></div>
-            <img src="${pokeImg}" alt="${pokeName}">
+            <div class="pokeTypeContainer" id="type${i}"></div>
+            <img class='pokeImg' src="${pokeImg}" alt="${pokeName}">
         </div>
     </div>`;
 
     for (let j = 0; j < pokemonAsJson["types"].length; j++) {
       pokeType = pokemonAsJson["types"][j]["type"][`name`];
-      renderType(pokeType, i);
+      if (j == 0) {
+        document.getElementById(
+          `type${i}`
+        ).innerHTML += `<div class='pokeType'>${pokeType}</div>`;
+        document
+          .getElementById(`pokeCard${i}`)
+          .classList.add(`box-shadow-${color}`);
+      }
     }
   }
-}
-
-function renderType(pokeType, i) {
-  document.getElementById(`type${i}`).innerHTML += `${pokeType}<br>`;
 }
 
 function openSearch() {
@@ -73,4 +78,60 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("nav-normal").classList.remove("d-none");
   document.getElementById("nav-search").classList.add("d-none");
+}
+
+// FOTOGALERIE
+
+function loadPokeValues(pokeId, pokeName) {
+  document.getElementById("img-click").classList.remove("d-none");
+  document.getElementById("img-click").innerHTML = /*html*/ `
+
+<div id="img-click-close" class="img-click" onclick="closeImg()">
+    <div class="img-load-head">
+      <img src="./img/cross.png" alt="close image" class="close-img" id="closeimg"/>
+    </div>
+    <div class="img-load">
+      <div class="content-flex">
+                <img src="./img/prev.png" alt="previous image" class="img-load-arrows" onclick="loadPrevImg(${pokeId})"/> 
+      <div class="poke-content">
+        <div class="card" id="pokeCard${pokeId}">
+          <div class="card-head">
+            <div class='pokeName' id="${pokeName}">${pokeName}</div>
+            <div class='pokeId' id="${pokeId}">#${pokeId}</div>
+          </div>
+        <div class="card-content">
+          <div class="pokeTypeContainer" id="type${pokeId}"></div>
+            
+          </div>
+      </div>
+      </div>
+<img src="./img/next.png" alt="next image" class="img-load-arrows" onclick="loadNextImg(${pokeId})"/>
+      </div>
+      </div>
+</div>`;
+}
+
+function closeImg() {
+  document.getElementById("img-click-close").classList.add("d-none");
+  document.getElementById("img-click").classList.add("d-none");
+}
+
+function loadNextImg(i) {
+  if (i == maxPokemonRender-1) {
+    i = 1;
+  } else {
+    i++;
+  }
+  loadPokeValues(i);
+  event.stopPropagation();
+}
+
+function loadPrevImg(i) {
+  if (i == 1) {
+    i = maxPokemonRender - 1;
+  } else {
+    i--;
+  }
+  loadPokeValues(i);
+  event.stopPropagation();
 }
