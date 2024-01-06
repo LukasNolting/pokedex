728ccff97f74d84919da2eb3c3d21d0e1fa78eab
Lukas Nolting <lukas.nolting@icloud.com>
2024-01-06 14:16:14 +0100

    20240106

--- script/script.js
+++ script/script.js
@@ -1,87 +1,139 @@
-let responseAsJson = ``;
-let maxPokemonRender = 21;
-let loadPokemonNumber = 1;
-let pokemonAsJson = [];
-let pokemons = [];
+let PokeIdURL = [];
+let PokeName = [];
+let PokeTypes = [];
+let pokemons = 0;
+let pokemonAsJsonComp = [];
+let currentPokemon = [];
 
-async function loadAPI(index) {
-  let url = `https://pokeapi.co/api/v2/pokemon?offset=${index}&limit=1400`;
+async function loadAPI() {
+  let url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1302";
   let response = await fetch(url);
-  responseAsJson = await response.json();
-  loadPokemon(1);
-  renderScreenInfos();
-}
-
-async function loadPokemon(index) {
-  let urlPokemon = `https://pokeapi.co/api/v2/pokemon/${index}`;
-  let pokemon = await fetch(urlPokemon);
-  pokemonAsJson = await pokemon.json();
-  pokemons.push(pokemonAsJson);
-  // downloadAllPokemonData(1302);
-}
-
-async function renderScreenInfos() {
-  renderPokemonSmall(loadPokemonNumber);
-}
-
-function loadMore() {
-  let index = loadPokemonNumber + 20;
-  loadPokemonNumber = index;
-  maxPokemonRender = maxPokemonRender + 20;
-  renderPokemonSmall(index);
-}
-
-async function renderPokemonSmall(loadPokemonNumber) {
-  for (let i = loadPokemonNumber; i < maxPokemonRender; i++) {
-    await loadPokemon(i);
-    pokeName = pokemons[i]["name"].charAt(0).toUpperCase() + pokemons[i]["name"].slice(1);
-    pokeImg = pokemons[i]["sprites"]["other"]["official-artwork"]["front_default"];
-    pokeType = pokemons[i]["types"];
-    pokeId = pokemons[i]["id"];
-    let color = pokemons[i]["types"][0]["type"]["name"];
-    let card = document.getElementById("card-container");
-    card.innerHTML += cardPokemonSmall(pokeName, pokeId, i, pokeImg);
-
-    for (let j = 0; j < pokemons[i]["types"].length; j++) {
-      pokeTypesAll =  pokemons[i]["types"][j]["type"][`name`].charAt(0).toUpperCase() + pokemons[i]["types"][j]["type"][`name`].slice(1);
-      document.getElementById(`type${i}`).innerHTML +=
-        cardPokeTypesSmall(pokeTypesAll);
-      if (j == 0) {
-        document
-          .getElementById(`pokeCard${i}`)
-          .classList.add(`box-shadow-${color}`);
-      }
-    }
+  let responseAsJson = await response.json();
+  let pokemonList = responseAsJson["results"];
+  for (let i = 0; i < pokemonList.length; i++) {
+    PokeIdURL.push(pokemonList[i]["url"]);
+    PokeName.push(pokemonList[i]["name"]);
   }
+  loadPokemon();
 }
 
-function renderPokemonBig(pokeId) {
-  let color = pokemons[pokeId]["types"][0]["type"]["name"];
-  pokeType = pokemons["types"];
-  pokeName =  pokemons[pokeId]["name"].charAt(0).toUpperCase() + pokemons[pokeId]["name"].slice(1);
-  pokeImg = pokemons[pokeId]["sprites"]["other"]["official-artwork"]["front_default"];
-  document.getElementById("img-click").classList.remove("d-none");
-  document.getElementById("img-click").innerHTML = cardPokemonBig(pokeId,pokeImg,pokeName);
+async function loadPokemon() {
+  for (let i = 0; i < 20; i++) {
+    let url = PokeIdURL[i];
+    let response = await fetch(url);
+    let pokemonAsJSON = await response.json();
+    pokemonAsJsonComp.push(pokemonAsJSON);
+    renderPokemon(i);
+  }
+  pokemons = 20;
+}
 
-  for (let j = 0; j < pokemons[pokeId]["types"].length; j++) {
-    pokeType =
-      pokemons[pokeId]["types"][j]["type"][`name`].charAt(0).toUpperCase() +
-      pokemons[pokeId]["types"][j]["type"][`name`].slice(1);
-    document.getElementById(`type-big${pokeId}`).innerHTML += cardPokeTypesBig(pokeType);
-    if (j == 0) {
-      document.getElementById(`pokeCardBig${pokeId}`).classList.add(`box-shadow-${color}`);
-    }
+async function loadMore(morePokemon) {
+  for (let i = pokemons; i < pokemons + morePokemon; i++) {
+    const url = PokeIdURL[i];
+    let response = await fetch(url);
+    let pokemonAsJSON = await response.json();
+    pokemonAsJsonComp.push(pokemonAsJSON);
+    renderPokemon(i);
+  }
+  pokemons += morePokemon;
+}
+
+function renderPokemon(i) {
+  let pokeName =
+    pokemonAsJsonComp[i]["name"].charAt(0).toUpperCase() +
+    pokemonAsJsonComp[i]["name"].slice(1);
+  let pokeId = pokemonAsJsonComp[i]["id"];
+  let pokeTypes = pokemonAsJsonComp[i]["types"];
+  let pokeType = [];
+  let pokeImg =
+    pokemonAsJsonComp[i]["sprites"]["other"]["official-artwork"][
+      "front_default"
+    ];
+  let color = pokemonAsJsonComp[i]["types"][0]["type"]["name"];
+
+  for (let i = 0; i < pokeTypes.length; i++) {
+    let pokeTypeElement = pokeTypes[i]["type"]["name"];
+    pokeType += cardPokeTypesSmall(pokeTypeElement);
+  }
+  document.getElementById("card-container").innerHTML += renderPokemonSmallCard(
+    pokeId,
+    pokeName,
+    pokeType,
+    pokeImg,
+    i
+  );
+  document.getElementById(`pokeCard${i}`).classList.add(`box-shadow-${color}`);
+}
+
+function renderPokemonBig(i) {
+  let pokeName =
+    pokemonAsJsonComp[i]["name"].charAt(0).toUpperCase() +
+    pokemonAsJsonComp[i]["name"].slice(1);
+  let pokeId = pokemonAsJsonComp[i]["id"];
+  let pokeTypes = pokemonAsJsonComp[i]["types"];
+  let pokeType = [];
+  let pokeImg =
+    pokemonAsJsonComp[i]["sprites"]["other"]["official-artwork"][
+      "front_default"
+    ];
+  let color = pokemonAsJsonComp[i]["types"][0]["type"]["name"];
+  for (let i = 0; i < pokeTypes.length; i++) {
+    let pokeTypeElement = pokeTypes[i]["type"]["name"];
+    pokeType += cardPokeTypesBig(pokeTypeElement);
+  }
+  document.getElementById("container-big").innerHTML = 
+  renderPokemonBigCard(pokeId,pokeImg,pokeName,pokeType,i);
+  document.getElementById(`pokeCardBigColor${i}`).classList.add(`box-shadow-${color}`);
+  loadMoreLast(i);
+  renderOverview(i);
+}
+
+function renderOverview(i){
+      let height = pokemonAsJsonComp[`${i}`]["height"] / 10;
+      let weight = pokemonAsJsonComp[`${i}`]["weight"] / 10;
+      let abilities = pokemonAsJsonComp[i]["abilities"]
+      let ability = [];
+      for (let j = 0; j < abilities.length; j++) {
+        let pokeAbilities =
+          abilities[j]["ability"]["name"].charAt(0).toUpperCase() +
+          abilities[j]["ability"]["name"].slice(1);
+        ability += renderOverviewAbilities(pokeAbilities);
+      }
+      document.getElementById("card-overview").innerHTML = /*html*/ `
+      <div class="card-overview">
+      <div class="flex-align-center-gap10"><img src="../img/height.svg" class="icon-big-card">Height: ${height} m</div>
+      <div class="flex-align-center-gap10"><img src="../img/weight.svg" class="icon-big-card">Weight: ${weight} kg</div>
+      <div class="flex-align-center-gap10">
+      Abilities: 
+      <div class="flex-column">${ability}</div>
+      </div>
+      </div>
+      `;    
+}
+
+function renderOverviewAbilities(pokeAbilities){
+    return /*html*/ `
+    <div class="abilities-ow"><img src="../img/pokeball.png" class="icon-big-card"> ${pokeAbilities}</div>
+    `;
+}
+
+function loadMoreLast(i){
+  if (i == pokemonAsJsonComp.length - 1){
+    loadMore(2);
+  } else {
+    loadMore(1);
   }
 }
 
 function closeImg() {
-  document.getElementById("img-click-close").classList.add("d-none");
-  document.getElementById("img-click").classList.add("d-none");
+  document.getElementById("pokeCardBig").classList.add("d-none");
+  document.getElementById("container-big").classList.add("d-none");
 }
 
 function loadNextPokemon(i) {
-  if (i == maxPokemonRender - 1) {
-    i = 1;
+  if (i == pokemonAsJsonComp.length - 1) {
+  loadMore(1);
   } else {
     i++;
   }
@@ -90,26 +142,11 @@ function loadNextPokemon(i) {
 }
 
 function loadPrevPokemon(i) {
-  if (i == 1) {
-    i = maxPokemonRender - 1;
+  if (i == 0) {
+    i = pokemonAsJsonComp.length - 1;
   } else {
     i--;
   }
   renderPokemonBig(i);
   event.stopPropagation();
 }
-
-
-
-//  TEST
-
-// let pokemonDataComplete = [];
-
-// async function downloadAllPokemonData() {
-//   // Für jedes Pokémon die Daten abrufen
-//   for (let i = 0; i <= responseAsJson.count; i++) {
-//     let response = await fetch(responseAsJson["results"][i]["url"]);
-//     let pokemonData = await response.json();
-//     pokemonDataComplete.push(await pokemonData);
-//   }
-// }
--- script/templates.js
+++ script/templates.js
@@ -1,13 +1,13 @@
 // Render small Cards onload
 
-function cardPokemonSmall(pokeName, pokeId, i, pokeImg) {
-  return /*html*/ `<div class="card-small" id="pokeCard${pokeId}" onclick="renderPokemonBig(${pokeId})">
+function renderPokemonSmallCard(pokeId, pokeName, pokeType, pokeImg, i) {
+  return /*html*/ `<div class="card-small" id="pokeCard${i}" onclick="renderPokemonBig(${i})">
         <div class="card-head">
-            <div class='pokeName' id="${pokeName}">${pokeName}</div>
-            <div class='pokeId' id="${pokeId}">#${pokeId}</div>
+            <div class='pokeName' id="nameSmall${pokeName}">${pokeName}</div>
+            <div class='pokeId' id="idSmall${pokeId}">#${pokeId}</div>
         </div>
         <div class="card-content-small">
-            <div class="pokeTypeContainer" id="type${i}"></div>
+            <div class="pokeTypeContainer">${pokeType}</div>
             <img class='pokeImg' src="${pokeImg}" alt="${pokeName}">
         </div>
     </div>`;
@@ -15,53 +15,59 @@ function cardPokemonSmall(pokeName, pokeId, i, pokeImg) {
 
 // Render Types / Classes on small Cards
 
-function cardPokeTypesSmall(pokeType){
-    return /*html*/ `<div class='pokeType'>${pokeType}</div>`;
+function cardPokeTypesSmall(pokeType) {
+  return /*html*/ `<div class='pokeType'>${pokeType}</div>`;
 }
 
 // Render big Cards onclick
 
-function cardPokemonBig(pokeId, pokeImg, pokeName) {
-  return /*html*/ `<div id="img-click-close" class="img-click">
-<img src="./img/prev.png" alt="previous" class="img-load-arrows" onclick="loadPrevPokemon(${pokeId}, '${pokeImg}', '${pokeName}')"/>  
-      
-<div class="card text-center" id="pokeCardBig${pokeId}">
-  <div class="card-header">
-    <ul class="nav nav-tabs card-header-tabs">
-            <li class="nav-item">
-              <a class="nav-link active" onclick="loadOverview()">Overview</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="loadStats()">Stats</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="loadAbilities()">Abilities</a>
-            </li>
-          </ul>
-        </div>
-        <div class="card-body">
-            <div class="flex-sb">
-          <h5 class="card-title" id="${pokeName}">Name: ${pokeName}</h5>
-          <div class='pokeId' id="${pokeId}">#${pokeId}</div>
-          </div>
-                <div class="card-content">
-                    <img class=pokeImg-big src="${pokeImg}" alt="${pokeId}">
-                    <div class="pokeTypeContainer-big" id="type-big${pokeId}"></div>
-                </div>
-        </div>
+function renderPokemonBigCard(pokeId, pokeImg, pokeName, pokeType, i) {
+  document.getElementById("container-big").classList.remove("d-none");
+  return /*html*/ `<div id="pokeCardBig" class="show-big">
+  <img
+    src="./img/prev.png"
+    alt="previous"
+    class="img-load-arrows"
+    onclick="loadPrevPokemon(${i})"
+  />
+  <div class="card text-center" id="pokeCardBigColor${i}">
+    <div class="card-header">
+      <ul class="nav nav-tabs card-header-tabs">
+        <li class="nav-item">
+          <a class="nav-link active" onclick="loadOverview()">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" onclick="loadStats()">Stats</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" onclick="loadAbilities()">Moves</a>
+        </li>
+      </ul>
     </div>
-    <img src="./img/next.png" alt="next image" class="img-load-arrows" onclick="loadNextPokemon(${pokeId}, '${pokeImg}', '${pokeName}')"/>
+    <div class="card-body">
+      <div class="flex-sb">
+        <h5 class="card-title" id="name-big${pokeName}">${pokeName}</h5>
+        <div class="pokeIdBig" id="id-big${pokeId}">#${pokeId}</div>
       </div>
+      <div class="card-content">
+        <img class="pokeImg-big" src="${pokeImg}" alt="${pokeId}" />
+        <div class="pokeTypeContainerBig">${pokeType}</div>
+      </div>
+      <div id="card-overview">
+        
+      </div>
+    </div>
+  </div>
+  <img
+    src="./img/next.png"
+    alt="next image"
+    class="img-load-arrows"
+    onclick="loadNextPokemon(${i})"
+  />
 </div>
-</div>`;
+`;
 }
 
-function loadOverview(){
-
-}
-
-// Render Types / Classes on big Cards
-
-function cardPokeTypesBig(pokeType) {
-  return /*html*/ `<div class='pokeType-big'>${pokeType}</div>`;
+function cardPokeTypesBig(pokeTypes) {
+  return /*html*/ `<div class='pokeType-big'>${pokeTypes}</div>`;
 }
